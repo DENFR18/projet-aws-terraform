aad35764866cd5f4964d736d729245d3b5ebe312
PtiDen 95 <denilsson.varelq@gmail.com>
2026-03-13 14:24:58 +0100

    fix: trivy ignore AVD-AWS-0164 subnet public IP (choix assumé)

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -12,7 +12,7 @@ resource "aws_internet_gateway" "igw" {
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = true
+  map_public_ip_on_launch = true #trivy:ignore:AVD-AWS-0164
   availability_zone       = "${var.region}a"
   tags                    = { Name = "subnet-public-${var.env}" }
 }
