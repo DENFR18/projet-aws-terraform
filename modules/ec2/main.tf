2fdd6735c7523f59801a7db71f8c664123011ed7
PtiDen 95 <denilsson.varelq@gmail.com>
2026-03-13 17:41:31 +0100

    fix: trivy ignore AVD-AWS-0104 egress (nécessaire pour les mises à jour)

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -37,7 +37,7 @@ resource "aws_security_group" "ec2_sg" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"] #trivy:ignore:AVD-AWS-0104
   }
 
   tags = { Name = "sg-ec2-${var.env}" }
